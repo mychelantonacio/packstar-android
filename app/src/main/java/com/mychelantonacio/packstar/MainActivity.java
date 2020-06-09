7fa9464e026037e75cd6b8b721923bd0e8249b3b
mplatini <mychel.platini2@gmail.com>
2020-06-09 22:45:20 +0100

    Initial commit

---
+++ app/src/main/java/com/mychelantonacio/packstar/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mychelantonacio.packstar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mychelantonacio.packstar.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
