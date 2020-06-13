6bce5532aecbbb6d060e0df3b1bd4f16bd10c77b
mplatini <mychel.platini2@gmail.com>
2020-06-13 21:46:18 +0100

    Style - replaced Base application theme.
    Home Screen - set full screen.

--- app/src/main/java/com/mychelantonacio/packstar/MainActivity.java
+++ app/src/main/java/com/mychelantonacio/packstar/MainActivity.java
@@ -3,15 +3,24 @@ package com.mychelantonacio.packstar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
+        setTheme(R.style.Theme_App);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupUIOnCreate();
     }
 
+    private void setupUIOnCreate() {
+        //no title bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+    }
 }
